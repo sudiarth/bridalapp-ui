65a0499ee7eb3d9a95c3b221ae6c6c864163aede
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-02-01 15:16:11 +0100

    Bypass proxy when on server side.

--- src/components/App/api.jsx
+++ src/components/App/api.jsx
@@ -1,4 +1,5 @@
-﻿import Api, { link } from 'redux-apis';
+﻿import log from 'picolog';
+import Api, { link } from 'redux-apis';
 import { remote, endpoint } from 'redux-fetch-api';
 
 import DrawerApi   from '../RightDrawer/api';
@@ -7,11 +8,12 @@ import ProductsApi from '../Products/api';
 
 let API_URL = '/api';
 if (typeof process != 'undefined') {
-	const host = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-	const port = process.env.OPENSHIFT_NODEJS_PORT || '80';
+	const host = process.env.BRIDALAPP_API_SERVER_HOST || 'localhost';
+	const port = process.env.BRIDALAPP_API_SERVER_PORT || '80';
 	const path = process.env.BRIDALAPP_API_SERVER_PATH || '/api';
 	API_URL = 'http://' + host + (port === '80' ? '' : ':' + port) + path;
 }
+log.log('Using REST API server url: ', API_URL);
 
 @remote(API_URL)
 export class AppApi extends Api {
