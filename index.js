5cc24322d7ef1f292c0b10dc84a710a8c9416cb3
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-03-11 00:27:55 +0100

    .

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-	res.end('<!DOCTYPE html><html><head><title>Status</title></head><body><h1 style="color:green">ONLINE</h1><p>' + cfg.server.name + ' is ONLINE</p></body></html>');
+	res.end('<!DOCTYPE html><html><head><title>Status</title></head><body><h1 style="color:green">ONLINE</h1><p>BridalApp is ONLINE</p></body></html>');
 });
 
 app.listen(app.get('port'), function() {
